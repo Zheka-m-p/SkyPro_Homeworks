616816ad767dc7800984a6dc62d0461cf2a8431b
Evgeniy <96652625+Zheka-m-p@users.noreply.github.com>
2022-12-15 08:07:40 +0300

    Update task2.py

---
+++ tasks/task2.py
@@ -0,0 +1,29 @@
+class Item:
+    def __init__(self, name, price, quantity=0):
+        
+        self.__check(name, price, quantity)
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+        
+    def __check(self, name, price, quantity):
+        if not isinstance(name, str):
+            raise TypeError('Название товара должно быть строкой.')
+        if not isinstance(price, (float, int)):
+            raise TypeError('Цена товара должна быть числом.')
+        if not isinstance(quantity, int):
+            raise TypeError('Количество товара должно быть целым числом.')
+
+    def __str__(self):
+         return f'{type(self).__name__}({self.name}, {self.price}, {self.quantity})'
+
+class Phone(Item):
+    def __init__(self):
+        #self.broken_phones = broken_phones
+        super().__init__(1)
+
+
+#phone1 = Phone("iPhone 10", 500, 5)
+# print(phone1)
+phone2 = Phone("iPhone 10", 500, 5, 1)
+print(phone2)
