46493afdb4b6e11979efce8a25261976e217442c
Evgeniy <96652625+Zheka-m-p@users.noreply.github.com>
2022-12-16 17:01:20 +0300

    Update les_01.py

--- lessons/les_01.py
+++ lessons/les_01.py
@@ -1,1 +1,20 @@
+class Readfile:
+    def __init__(self, file, mode):
+        print('__init__')
+        self.file = file
+        self.mode = mode
+        
+    def __enter__(self):
+        print('__enter__')
+        self.f = open(self.file, self.mode)
+        return self.f
 
+    def __exit__(self, ex_type, ex_val, ex_tb):
+        print('__exit__')
+        self.f.close()
+
+with Readfile('sample.txt', 'r') as f:
+    print('some code')
+    text = f.read()
+    print(text)
+    
