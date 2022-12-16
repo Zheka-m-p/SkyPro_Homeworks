57d4f7019c381f2037cedd870787eafcd451eefd
Evgeniy <96652625+Zheka-m-p@users.noreply.github.com>
2022-12-16 12:27:46 +0300

    Update task3.py

--- tasks/task3.py
+++ tasks/task3.py
@@ -1,1 +1,19 @@
+class InvalidAgeError(Exception):  # Зачем изоретать велосипед, если и так работает?)
+    pass
 
+#class InvalidAgeError(Exception):
+    #def __init__(self, *args):
+        #self.msg = args[0] if args else ""
+
+    #def __str__(self):
+        #return self.msg
+        
+try:
+    age = int(input('Введите ваш возраст: '))
+    if age < 0 or age >= 120:
+        raise InvalidAgeError('Извините, этот возраст не корректен')
+    print(f'Вам {age} лет')
+except ValueError:
+    print('Возраст должен быть числом')
+except InvalidAgeError as e:
+    print(e)
