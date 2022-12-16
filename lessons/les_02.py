64fb9495c33b524adeb8d296be8b0109ab2fe9b2
Evgeniy <96652625+Zheka-m-p@users.noreply.github.com>
2022-12-16 17:01:52 +0300

    Update les_02.py

--- lessons/les_02.py
+++ lessons/les_02.py
@@ -1,1 +1,23 @@
+class Laptop:
+    def __init__(self, brand, cpu):
+        self.brand = brand
+        self.memory = self.Memory()
+        self.cpu = cpu
 
+    def print_details(self):
+        print(self.brand, self.memory.size, self.cpu)
+
+    class Memory:
+        def __init__(self):
+            self.size = '8gb'
+
+# eight_gb = Memory()
+hp_laptop = Laptop('HP', 'i7 Processor')
+hp_laptop.memory.size = '4gb'
+hp_laptop.print_details()
+
+dell_laptop = Laptop('Dell', 'i5')
+dell_laptop.print_details()
+
+print(id(hp_laptop.memory))
+print(id(dell_laptop.memory))
