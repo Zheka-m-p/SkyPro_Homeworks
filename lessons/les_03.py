23477c4fc3c77c8b2c7e1b5d5f910bea734b9c62
Zheka-m-p <chernik.dota@mail.ru>
2022-12-18 17:43:29 +0300

    les_03 recorded

--- lessons/les_03.py
+++ lessons/les_03.py
@@ -1,1 +1,28 @@
+# class User:
+#     pass
+#
+# User.name = 'Bob'
+#
+# x = User()
+# x.name = 'Tim'
+class User:
+    __slots__ = ('name', 'age')
 
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        # self.fi = 1 # так тоже будет ругаца
+
+x = User('Bob', 40)
+
+User.__dict__
+
+x.__dict__
+
+x.weight = 80
+
+del x.name
+
+x.name
+
+x.name = 'Tim'
