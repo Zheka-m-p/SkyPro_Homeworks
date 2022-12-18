33292fba507d6b902cf86c2a0b5cfe38c163910a
Zheka-m-p <chernik.dota@mail.ru>
2022-12-18 16:09:41 +0300

    les-01

--- lessons/les_01.py
+++ lessons/les_01.py
@@ -1,1 +1,43 @@
+class Animal:
+    @staticmethod
+    def make_sound():
+        raise NotImplementedError('Метод make_sound() должен быть переопределен в дочернем классе')
+
+class Cat(Animal):
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def info(self):
+        print(f"cat {self.name}, {self.age} years old")
+
+    @staticmethod
+    def make_sound():
+        print('Meow')
+
+class Dog(Animal):
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def info(self):
+        print(f"dog {self.name}, {self.age} years old")
+
+    @staticmethod
+    def make_sound():
+        print('Gav')
+
+cat1 = Cat('Kitty', 10)
+dog1 = Dog('Sharik', 4)
+#
+# cat1.cat_info()
+# cat1.make_meow()
+#
+# dog1.dog_info()
+# dog1.make_gav()
+
+for animal in (cat1, dog1):
+    animal.info()
+    animal.make_sound()
+
 
